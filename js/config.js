20d5a131fb99550d229b4b1342b1f6fee98291b6
FelipeUsuga1 <pipeusuga48501@gmail.com>
2026-07-13 14:48:35 -0500

    Se cambia el valor nightlong

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@ export const CONFIG=Object.freeze({
   player:{width:82,height:88,duckWidth:88,duckHeight:48,x:90,gravity:2350,jumpVelocity:-890,runFrameTime:.11,hitbox:{x:8,y:6,right:9,bottom:4},duckHitbox:{x:8,y:7,right:9,bottom:4}},
   obstacle:{initialGap:450,minGap:330,maxGap:590,gapReduction:.075,spawnPadding:24,pteroUnlockScore:80,pteroWidth:76,pteroHeight:48,pteroLowOffset:8,pteroHighOffset:55,animTime:.18},
   game:{initialSpeed:330,maxSpeed:650,acceleration:8,scoreRate:.025,recordFlashTime:1.2},
-  cycle:{dayLength:500,transitionLength:10,nightLength:200,stars:18},
+  cycle:{dayLength:500,transitionLength:10,nightLength:500,stars:18},
   audio:{jumpFrequency:470,recordFrequency:720,collisionFrequency:105,volume:.07},colors:{ink:'#484848',paper:'#ffffff',nightInk:'#f2f2f2',nightPaper:'#202124'}
 });
